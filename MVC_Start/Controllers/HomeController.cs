4c63af2e5d52e84ff23118bb3d8577fae1ff6a26
Nynali Gopu <ngopu@usf.edu>
2021-04-25 04:48:06 -0400

    Added data to local database for program details table and made search operation active. Modified views with few changes as needed for this latest assignment.

--- MVC_Start/Controllers/HomeController.cs
+++ MVC_Start/Controllers/HomeController.cs
@@ -35,74 +35,9 @@ namespace MVC_Start.Controllers
             return View();
         }
 
-        //public IActionResult IndexWithLayout()
-        //{
-        //    return View();
-        //}
-
-        //public IActionResult Contact()
-        //{
-        //    GuestContact contact = new GuestContact();
-
-        //    contact.Name = "Manish Agrawal";
-        //    contact.Email = "magrawal@usf.edu";
-        //    contact.Phone = "813-974-6716";
-
-
-        //    /* alternate syntax to initialize object 
-        //    GuestContact contact2 = new GuestContact
-        //    {
-        //      Name = "Manish Agrawal",
-        //      Email = "magrawal@usf.edu",
-        //      Phone = "813-974-6716"
-        //    };
-        //    */
-
-        //    //ViewData["Message"] = "Your contact page.";
-
-        //    return View(contact);
-        //}
-
-        //[HttpPost]
-        //public IActionResult Contact(GuestContact contact)
-        //{
-        //    return View(contact);
-        //}
-
-        /// <summary>
-        /// Replicate the chart example in the JavaScript presentation
-        /// 
-        /// Typically LINQ and SQL return data as collections.
-        /// Hence we start the example by creating collections representing the x-axis labels and the y-axis values
-        /// However, chart.js expects data as a string, not as a collection.
-        ///   Hence we join the elements in the collections into strings in the view model
-        /// </summary>
-        /// <returns>View that will display the chart</returns>
-        //public ViewResult DemoChart()
-        //{
-        //    string[] ChartLabels = new string[] { "Africa", "Asia", "Europe", "Latin America", "North America" };
-        //    int[] ChartData = new int[] { 2478, 5267, 734, 784, 433 };
-
-        //    ChartModel Model = new ChartModel
-        //    {
-        //        ChartType = "bar",
-        //        Labels = String.Join(",", ChartLabels.Select(d => "'" + d + "'")),
-        //        Data = String.Join(",", ChartData.Select(d => d)),
-        //        Title = "Predicted world population (millions) in 2050"
-        //    };
-
-        //    return View(Model);
-
-
-
-        //}
-
-
-
         public IActionResult AboutUs()
         {
             return View();
-
 
         }
 
@@ -122,11 +57,6 @@ namespace MVC_Start.Controllers
         public IActionResult Searchuniv()
         {
             
-            //foreach(var schoolData in dbContext.Schools)
-            //{
-            //    dbContext.Schools.Remove(schoolData);
-            //}
-            //dbContext.SaveChanges();
             var schoolDataList = new List<SchoolData>();
             if (!dbContext.Schools.Any())
             {
@@ -135,8 +65,6 @@ namespace MVC_Start.Controllers
                 httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
                 httpClient.DefaultRequestHeaders.Accept.Add(
                     new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-
 
                 string SCHOOL_DATA_API_PATH = BASE_URL + "schools?fields=id,ope6_id,school.name,school.city,school.state,school.zip";
 
@@ -196,6 +124,86 @@ namespace MVC_Start.Controllers
             }
             ViewBag.Schools = schoolDataList;
             schoolsInformation = schoolDataList;
+
+            //backup
+
+            if (!dbContext.Programs.Any())
+            {
+                dbContext.Database.EnsureCreated();
+                ProgramData[] plist = new ProgramData[]
+                    {
+                
+
+                     new ProgramData{code="1", title="Management Sciences and Quantitative Methods", ope6_id="001002", unit_id=1 },
+                     new ProgramData { code = "2", title = "Journalism", ope6_id = "001002", unit_id = 2 },
+                     new ProgramData { code = "3", title = "Entrepreneurial and Small Business Operations", ope6_id = "001002", unit_id = 3 },
+                     new ProgramData { code = "4", title = "Public Relations, Advertising, and Applied Communication", ope6_id = "001003", unit_id = 4 },
+                     new ProgramData { code = "5", title = "Geological and Earth Sciences/Geosciences", ope6_id = "001003", unit_id = 5 },
+                     new ProgramData { code = "6", title = "International Business", ope6_id = "001003", unit_id = 6 },
+                     new ProgramData { code = "7", title = "Communication and Media Studies", ope6_id = "041872", unit_id = 7 },
+                     new ProgramData { code = "8", title = "Computer/Information Technology Administration and Management", ope6_id = "041872", unit_id = 8 },
+                     new ProgramData { code = "9", title = "Food Science and Technology", ope6_id = "041872", unit_id = 9 },
+                     new ProgramData { code = "10", title = "Parks, Recreation and Leisure Facilities Management", ope6_id = "001017", unit_id = 10 },
+                     new ProgramData { code = "11", title = "Family and Consumer Sciences/Human Sciences, General", ope6_id = "001017", unit_id = 11 },
+                     new ProgramData { code = "12", title = "Business, Management, Marketing, and Related Support Services, Other", ope6_id = "001017", unit_id = 12 },
+                     new ProgramData { code = "13", title = "Computer Systems Networking and Telecommunicationss", ope6_id = "001060", unit_id = 13 },
+                     new ProgramData { code = "14", title = "Computer Software and Media Applications", ope6_id = "001060", unit_id = 14 },
+                     new ProgramData { code = "15", title = "Vehicle Maintenance and Repair Technologies", ope6_id = "001060", unit_id = 15 },
+                     new ProgramData { code = "16", title = "Management Sciences and Quantitative Methods", ope6_id = "001015", unit_id = 16 },
+                     new ProgramData { code = "17", title = "Journalism", ope6_id = "001015", unit_id = 17 },
+                     new ProgramData { code = "18", title = "Entrepreneurial and Small Business Operations", ope6_id = "001015", unit_id = 18 },
+                     new ProgramData { code = "19", title = "Public Relations, Advertising, and Applied Communication", ope6_id = "013039", unit_id = 19 },
+                     new ProgramData { code = "20", title = "Geological and Earth Sciences/Geosciences", ope6_id = "013039", unit_id = 20 },
+                     new ProgramData { code = "21", title = "International Business", ope6_id = "013039", unit_id = 21 },
+                     new ProgramData { code = "22", title = "Communication and Media Studies", ope6_id = "012182", unit_id = 22 },
+                     new ProgramData { code = "23", title = "Computer/Information Technology Administration and Management", ope6_id = "012182", unit_id = 23 },
+                     new ProgramData { code = "24", title = "Food Science and Technology", ope6_id = "012182", unit_id = 24 },
+                     new ProgramData { code = "25", title = "Parks, Recreation and Leisure Facilities Management", ope6_id = "001012", unit_id = 25 } ,
+                     new ProgramData { code = "26", title = "Family and Consumer Sciences/Human Sciences, General", ope6_id = "001012", unit_id = 26 },
+                     new ProgramData { code = "27", title = "Business, Management, Marketing, and Related Support Services, Other", ope6_id = "001012", unit_id = 27 },
+                     new ProgramData { code = "28", title = "Computer Systems Networking and Telecommunicationss", ope6_id = "001009", unit_id = 28 },
+                     new ProgramData { code = "29", title = "Computer Software and Media Applications", ope6_id = "001009", unit_id = 29 },
+                     new ProgramData { code = "30", title = "Vehicle Maintenance and Repair Technologies", ope6_id = "001009", unit_id = 30 },
+                     new ProgramData { code = "31", title = "Management Sciences and Quantitative Methods", ope6_id = "008310", unit_id = 31 },
+                     new ProgramData { code = "32", title = "Journalism", ope6_id = "008310", unit_id = 32 },
+                     new ProgramData { code = "33", title = "Entrepreneurial and Small Business Operations", ope6_id = "008310", unit_id = 33 },
+                     new ProgramData { code = "34", title = "Public Relations, Advertising, and Applied Communication", ope6_id = "001008", unit_id = 34 },
+                     new ProgramData { code = "35", title = "Geological and Earth Sciences/Geosciences", ope6_id = "001008", unit_id = 35 },
+                     new ProgramData { code = "36", title = "International Business", ope6_id = "001008", unit_id = 36 },
+                     new ProgramData { code = "37", title = "Communication and Media Studies", ope6_id = "001007", unit_id = 37 },
+                     new ProgramData { code = "38", title = "Computer/Information Technology Administration and Management", ope6_id = "001007", unit_id = 38 },
+                     new ProgramData { code = "39", title = "Food Science and Technology", ope6_id = "001007", unit_id = 39 },
+                     new ProgramData { code = "40", title = "Parks, Recreation and Leisure Facilities Management", ope6_id = "001051", unit_id = 40 },
+                     new ProgramData { code = "41", title = "Family and Consumer Sciences/Human Sciences, General", ope6_id = "001051", unit_id = 41 },
+                     new ProgramData { code = "42", title = "Business, Management, Marketing, and Related Support Services, Other", ope6_id = "001051", unit_id = 42 },
+                     new ProgramData { code = "43", title = "Computer Systems Networking and Telecommunicationss", ope6_id = "001005", unit_id = 43 },
+                     new ProgramData { code = "44", title = "Computer Software and Media Applications", ope6_id = "001005", unit_id = 44 },
+                     new ProgramData { code = "45", title = "Vehicle Maintenance and Repair Technologies", ope6_id = "001005", unit_id = 45 },
+                     new ProgramData { code = "46", title = "Management Sciences and Quantitative Methods", ope6_id = "001055", unit_id = 46 },
+                     new ProgramData { code = "47", title = "Journalism", ope6_id = "001055", unit_id = 47 },
+                     new ProgramData { code = "48", title = "Entrepreneurial and Small Business Operations", ope6_id = "001055", unit_id = 48 },
+                     new ProgramData { code = "49", title = "Public Relations, Advertising, and Applied Communication", ope6_id = "025034", unit_id = 49 },
+                     new ProgramData { code = "50", title = "Geological and Earth Sciences/Geosciences", ope6_id = "025034", unit_id = 50 },
+                     new ProgramData { code = "51", title = "International Business", ope6_id = "025034", unit_id = 51 },
+                     new ProgramData { code = "52", title = "Communication and Media Studies", ope6_id = "001052", unit_id = 52 },
+                     new ProgramData { code = "53", title = "Computer/Information Technology Administration and Management", ope6_id = "001052", unit_id = 53 },
+                     new ProgramData { code = "54", title = "Food Science and Technology", ope6_id = "001052", unit_id = 54 },
+                     new ProgramData { code = "55", title = "Parks, Recreation and Leisure Facilities Management", ope6_id = "001018", unit_id = 55 },
+                     new ProgramData { code = "56", title = "Family and Consumer Sciences/Human Sciences, General", ope6_id = "001018", unit_id = 56 },
+                     new ProgramData { code = "57", title = "Business, Management, Marketing, and Related Support Services, Other", ope6_id = "001018", unit_id = 57 },
+                     new ProgramData { code = "58", title = "Computer Systems Networking and Telecommunicationss", ope6_id = "007871", unit_id = 58 },
+                     new ProgramData { code = "59", title = "Computer Software and Media Applications", ope6_id = "007871", unit_id = 59 },
+                     new ProgramData { code = "60", title = "Vehicle Maintenance and Repair Technologies", ope6_id = "007871", unit_id = 60 },
+            
+            };
+                        foreach (ProgramData p in plist)
+                        { 
+                             dbContext.Programs.Add(p);
+                        }
+                              dbContext.SaveChanges();
+
+            }
+
             return View(schoolDataList);
         }
 
@@ -215,88 +223,16 @@ namespace MVC_Start.Controllers
         
         public IActionResult Programdetails(string id)
         {
-            //foreach (var  programData in dbContext.Programs)
-            //{
-            //    dbContext.Programs.Remove(programData);
-            //}
-            //dbContext.SaveChanges();
+            
+
             var programDataList = new List<ProgramData>();
-            //if (!dbContext.Programs.Any())
-            //{
-            //    httpClient2 = new HttpClient();
-            //    httpClient2.DefaultRequestHeaders.Accept.Clear();
-            //    httpClient2.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
-            //    httpClient2.DefaultRequestHeaders.Accept.Add(
-            //        new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
-            //    string Program_DATA_PARK_API_PATH = BASE_URL + "schools?fields=latest.programs.cip_4_digit.code,latest.programs.cip_4_digit.ope6_id,latest.programs.cip_4_digit.unit_id,latest.programs.cip_4_digit.title&per_page=100";
+                programDataList = dbContext.Programs.ToList();
+                ViewBag.Programs = programDataList.Where(x => x.ope6_id == id).ToList();
+                return View(programDataList);
 
-
-
-            //    string programdata = "";
-
-
-
-            //    Programs programs = null;
-
-            //    httpClient2.BaseAddress = new Uri(Program_DATA_PARK_API_PATH);
-
-            //    try
-            //    {
-            //        HttpResponseMessage response2 = httpClient2.GetAsync(Program_DATA_PARK_API_PATH).GetAwaiter().GetResult();
-
-            //        if (response2.IsSuccessStatusCode)
-            //        {
-            //            programdata = response2.Content.ReadAsStringAsync().GetAwaiter().GetResult();
-            //            programdata = replaceString(programdata);
-
-            //        }
-
-            //        if (!programdata.Equals(""))
-            //        {
-
-            //            programs = JsonConvert.DeserializeObject<Programs>(programdata);
-            //            var i = 1;
-
-            //            programs.results.ToList().ForEach(r =>
-            //            {
-            //                r.data.ToList().ForEach(p =>
-            //                {
-            //                    var program = new ProgramData()
-            //                    {
-            //                    //ID = i++,
-            //                    code = p.code,
-            //                        title = p.title,
-            //                        unit_id = p.unit_id,
-            //                        ope6_id = p.ope6_id,
-
-
-            //                    };
-
-
-            //                    dbContext.Programs.Add(program);
-            //                });
-            //            });
-            //            dbContext.SaveChanges();
-
-
-
-            //        }
-            //        programDataList = dbContext.Programs.ToList();
-            //    }
-            //    catch (Exception e)
-            //    {
-
-            //        Console.WriteLine(e.Message);
-            //    }
-            //}
-            //else
-            //{
-            //    programDataList = dbContext.Programs.ToList();
-            //}
-            programDataList = dbContext.Programs.ToList();
-            ViewBag.Programs = programDataList.Where(x=> x.ope6_id == id).ToList();
-            return View(programDataList);
+         
+         
         }
 
 
